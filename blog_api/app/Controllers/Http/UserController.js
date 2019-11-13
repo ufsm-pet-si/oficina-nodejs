785c85975c208050b60f456596ec254b7c0f0ee0
mtsdalmolin <mtsdalmolin@gmail.com>
2019-11-13 10:19:06 -0300

    update nas rotas e no controller do user

--- blog_api/app/Controllers/Http/UserController.js
+++ blog_api/app/Controllers/Http/UserController.js
@@ -5,10 +5,38 @@ const User = use("App/Models/User")
 class UserController {
     async create ({ request }) {
         const data = request.only(["username", "email", "password"])
-
         const user = await User.create(data)
-
         return user
+    }
+
+    async index ({ request, response, view }) {
+        const users = await User.all()
+        return users
+    }
+    
+    async store ({ request, response }) {
+        const id = request.input('id')
+        const data = request.only(['title', 'body'])
+        const user = await User.create({ ...data, user_id: id })
+        return user
+    }
+    
+    async show ({ params, request, response, view }) {
+        const user = await User.findOrFail(params.id)
+        return user
+    }
+    
+    async update ({ params, request, response }) {
+        const user = await User.findOrFail(params.id)
+        const data = request.only(['title', 'body'])
+        user.merge(data)
+        await user.save()
+        return user
+    }
+
+    async destroy ({ params, request, response }) {
+        const user = await User.findOrFail(params.id)
+        await user.delete()
     }
 }
 
--- blog_api/start/routes.js
+++ blog_api/start/routes.js
@@ -20,7 +20,7 @@ Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
 
-Route.post('/users', 'UserController.create')
+Route.resource('users', 'UserController')
 
 Route.resource('posts', 'PostController')
     .apiOnly()
